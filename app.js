55bcb3c7fc6d3c837c51d043baba1d2ca294b322
Ary Vidot <aryvid@gmail.com>
2020-03-20 23:08:10 +0000

    Style done

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-window.addEventListener("load", () => {
-  let long;
-  let lat;
-});
+const api = {
+  key: "0267d2e558931f4fc2e5b8964655cd54",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    // getResults(searchbox.value);
+    console.log(searchbox.value);
+  }
+}
